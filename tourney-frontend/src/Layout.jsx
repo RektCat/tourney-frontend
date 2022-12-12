35852b25af637cc54057616e8fe295804f3ab27f
Norbs <48733224+RektCat@users.noreply.github.com>
2022-12-12 16:46:43 +0100

    footer position fixed

--- tourney-frontend/src/Layout.jsx
+++ tourney-frontend/src/Layout.jsx
@@ -4,14 +4,14 @@ import Header from "./components/Header/Header";
 
 function Layout() {
   return (
-    <div className="flex min-h-screen flex-1 flex-col bg-gray-900">
-      <div className="mx-auto w-full max-w-[1250px]">
+    <div className="flex min-h-screen flex-col bg-gray-900">
+      <div className="mx-auto w-full max-w-[1250px] flex-1">
         <Header />
         <main className="block">
           <Outlet />
         </main>
       </div>
-      <footer className="absolute bottom-0 w-full border-t border-t-white bg-white/10 py-[2px] text-center text-xs">
+      <footer className="w-full border-t border-t-white bg-white/10 py-[2px] text-center text-xs">
         Created by <b>Norbert Tanács</b> for thesis
       </footer>
     </div>
